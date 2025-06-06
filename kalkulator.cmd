e817191f58ace313ba2880021c0160af2835f54f
Bouska <BouskaDo@vda.cz>
2025-06-06 10:30:23 +0200

    pridal jsem komentare a opravil podmínky

--- kalkulator.cmd
+++ kalkulator.cmd
@@ -1,13 +1,15 @@
 @echo off
 :start
 
+REM nacteni udaju
 set /p prvni_p=Zadej prvni cislo:
 set /p druha_p=Zadej druhe cislo:
 
+REM zjisteni typu vypoctu
 set /p typ=Zadej typ vypoctu(scitani,nasobeni,deleni):
-if %typ%=scitani goto scitani
-if %typ%=nasobeni goto nasobeni
-if %typ%=deleni goto deleni
+if %typ%==scitani goto scitani
+if %typ%==nasobeni goto nasobeni
+if %typ%==deleni goto deleni
 
 :scitani
 set vysledek=%prvni_p%+%druha_p%
